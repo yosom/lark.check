3cdf6636c4cb3d172a6150ac9faccb5839b5273a
json <weishuai.job@gmail.com>
2025-07-09 17:03:21 +0800

    add config

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # 检查数据是否准确
 # lark.check
+
+https://yosom.github.io/lark.check
--- vite.config.js
+++ vite.config.js
@@ -9,5 +9,6 @@ export default defineConfig({
   },
   build: {
     outDir: 'docs',
-  }
+  },
+  base: '/lark.check/',
 })
